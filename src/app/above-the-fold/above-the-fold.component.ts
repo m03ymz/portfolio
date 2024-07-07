fab9cd3e2b97f90dd32f1c2cca3c7da1a8a1b846
Muhammed Yilmaz <moe03@live.de>
2024-07-07 04:01:11 +0200

    responsive design

--- src/app/above-the-fold/above-the-fold.component.ts
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -38,41 +38,86 @@ export class AboveTheFoldComponent implements OnInit {
     });
   }
 
+  // getFontSize(): number {
+  //   const headingElement = document.getElementById('me');
+  //   if (headingElement) {
+  //     const textLength = headingElement.innerText.trim().length;
+  //     if (window.innerWidth <= 400) {
+  //       if (textLength > 5) {
+  //         return 18; 
+  //       } else {
+  //         return 26; 
+  //       }
+  //     } else if (window.innerWidth <= 525) {
+  //       if (textLength > 5) {
+  //         return 20; 
+  //       } else {
+  //         return 32; 
+  //       }
+  //     } else if (window.innerWidth <= 650) {
+  //       if (textLength > 5) {
+  //         return 24; 
+  //       } else {
+  //         return 32; 
+  //       }
+  //     } else if (window.innerWidth <= 1500) {
+  //       if (textLength > 5) {
+  //         return 30; 
+  //       } else {
+  //         return 40; 
+  //       }
+  //     } else if (window.innerWidth <= 1700 && textLength > 5) {
+  //       return 32; 
+  //     } else {
+  //       return 48; 
+  //     }
+  //   }
+  //   return 64; 
+  // }
+
   getFontSize(): number {
     const headingElement = document.getElementById('me');
     if (headingElement) {
       const textLength = headingElement.innerText.trim().length;
       if (window.innerWidth <= 400) {
         if (textLength > 5) {
-          return 22; // Schriftgröße 22px für Textlänge über 5 und Bildschirmbreiten bis 400px
+          return 16;
         } else {
-          return 26; // Schriftgröße 26px für Textlänge 5 oder weniger und Bildschirmbreiten bis 400px
+          return 26;
         }
       } else if (window.innerWidth <= 525) {
         if (textLength > 5) {
-          return 24; // Schriftgröße 24px für Textlänge über 5 und Bildschirmbreiten von 401px bis 525px
+          return 18;
         } else {
-          return 36; // Schriftgröße 36px für Textlänge 5 oder weniger und Bildschirmbreiten von 401px bis 525px
+          return 30;
         }
       } else if (window.innerWidth <= 650) {
         if (textLength > 5) {
-          return 28; // Schriftgröße 28px für Textlänge über 5 und Bildschirmbreiten von 526px bis 650px
+          return 24;
         } else {
-          return 36; // Schriftgröße 36px für Textlänge 5 oder weniger und Bildschirmbreiten von 526px bis 650px
+          return 32;
         }
       } else if (window.innerWidth <= 1500) {
         if (textLength > 5) {
-          return 38; // Schriftgröße 38px für Textlänge über 5 und Bildschirmbreiten von 651px bis 1500px
+          return 30;
         } else {
-          return 44; // Schriftgröße 44px für Textlänge 5 oder weniger und Bildschirmbreiten bis 1500px
+          return 46;
         }
-      } else if (window.innerWidth <= 1700 && textLength > 5) {
-        return 44; // Schriftgröße 44px für Textlänge über 5 und Bildschirmbreiten von 1501px bis 1700px
+      } else if (window.innerWidth <= 1700) {
+        if (textLength > 5) {
+          return 36;
+        } else {
+          return 54;
+        }
       } else {
-        return textLength > 5 ? 48 : 64; // Schriftgröße 48px für Textlänge 5 oder weniger oder größere Bildschirme
+        if (textLength > 5) {
+          return 48;
+        } else {
+          return 70;
+        }
       }
     }
-    return 64; // Standard-Schriftgröße, falls Element nicht gefunden wird oder leer ist
+    return 64; // Diese Zeile wird nur erreicht, wenn das headingElement nicht gefunden wird.
   }
 
   @HostListener('window:resize', ['$event'])
